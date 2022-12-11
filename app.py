d9dd38dd68d2646e03707416739c57f910fce4ce
CjSystemas <cjsystemas@gmail.com>
2022-12-11 17:17:00 -0300

    Confirmar

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
-
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def homepage():
-  return render_template("homepage.html")
+def index():
+  return render_template("index.html")
+
 
 @app.route("/contatos")
 def contatos():
   return render_template("contatos.html")
 
+
+@app.route("/sobre")
+def sobre():
+  return render_template("sobre.html")
+
+
 @app.route("/usuarios/<nome_usuario>")
 def usuarios(nome_usuario):
   return render_template("usuarios.html", nome_usuario=nome_usuario)
+
 
